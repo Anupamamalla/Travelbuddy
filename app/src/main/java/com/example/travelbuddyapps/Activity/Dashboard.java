01f0bd75e4b2e82eb2c3b5aa20b4ec77bdc834d9
anupama malla <Anupamamalla>
2020-01-29 14:18:00 +0545

    cardview added

--- app/src/main/java/com/example/travelbuddyapps/Activity/Dashboard.java
+++ app/src/main/java/com/example/travelbuddyapps/Activity/Dashboard.java
@@ -1,18 +1,59 @@
 package com.example.travelbuddyapps.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+
+import com.example.travelbuddyapps.Fragment.Search;
 import com.example.travelbuddyapps.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private NavigationView nv;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        toolbar = findViewById(R.id.toolbar);
+        getSupportActionBar().hide();
+        drawer = findViewById(R.id.drawer);
+        NavigationView nv = findViewById(R.id.nav_view);
+
+        //nv.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+
+
+
+
     }
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
+
--- app/src/main/java/com/example/travelbuddyapps/Activity/MainActivity.java
+++ app/src/main/java/com/example/travelbuddyapps/Activity/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.travelbuddyapps.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.travelbuddyapps.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
